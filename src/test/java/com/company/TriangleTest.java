ea8c8ceddc49cc29dd9225fa820643f2c9cd6c9d
dejda <jasiuczapiewski99@gmail,com>
2022-04-19 14:59:10 +0200

    3th commit

--- src/test/java/com/company/TriangleTest.java
+++ src/test/java/com/company/TriangleTest.java
@@ -7,7 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TriangleTest {
-//testtejhm
+//testtejhmnkj
+
 //    @Test
 //    void test(){
 //        //Given
